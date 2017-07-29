98a193293e4ad6663c587ed0a8841b6e8d7c24be
Cernan Bernardo <cernanb@gmail.com>
2017-07-28 23:25:59 -0700

    Add tests for households controllers and passing code

---
+++ app/controllers/households_controller.rb
@@ -0,0 +1,9 @@
+class HouseholdsController < ApplicationController
+
+  def index
+    @households = Household.all
+    # @households = current_user.households
+    render json: @households
+  end
+
+end
---
+++ spec/controllers/households_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe HouseholdsController, type: :controller do
+
+  before(:each) do
+    @user = create(:user)
+    @request.session[:user_id] = @user.id
+  end
+
+  describe 'GET index' do
+
+    it "returns all households that belong to the user" do
+      household1 = create(:household, user: @user)
+      user2 = User.create(first_name: "Ashley",
+                          last_name: "Bernardo",
+                          email: "ashley@gmail.com",
+                          password: "password")
+      household2 = Household.create(name: "Johnson Family",
+                                    address: "123 Main St",
+                                    monthly_rate: 3000,
+                                    user: user2)
+      get :index
+
+      expect(assigns[:households]).to include(household1)
+    end
+  end
+
+end
