67a3b02d438ccf6d26fe3a018fd0320217d264f7
Cernan Bernardo <cernanb@gmail.com>
2017-08-03 21:49:46 -0700

    Add specs for user requests

---
+++ spec/requests/api/v1/users_requests_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe "API::V1::Users", type: :request do
+
+  describe "POST /users" do
+
+    describe "on success" do
+
+      before(:each) do
+        params = {
+          user: {
+            email: "cernan@flatironschool.com",
+            password: "password"
+          }
+        }
+
+        post "/api/v1/users",
+          params: params.to_json,
+          headers: {'Content-Type': 'application/json'}
+        
+        @response = response
+      end
+
+      it "creates a user from the params" do
+        expect(User.all.count).to eq(1)  
+      end
+
+      it "returns the new user and a JWT token" do
+        body = JSON.parse(@response.body)
+
+        expect(response.status).to eq(200)
+        expect(body["user"]["id"]).not_to eq(nil)
+        expect(body["user"]["email"]).to eq("cernan@flatironschool.com")
+        expect(body["user"]["password_digest"]).to eq(nil)
+        expect(body['token']).not_to eq(nil)
+      end 
+    end
+
+    describe "on error" do
+
+      before(:each) do
+        User.create(
+          email: "cernan@flatironschool.com",
+          password: "password"                    
+        )
+      end
+
+      it "unable to find user with email" do
+        params = {
+          user: {
+            email: "cernan@thepub.com",
+            password: "password"
+          }
+        }
+
+        post "/api/v1/auth",
+          params: params.to_json,
+          headers: {'Content-Type': 'application/json'}
+        
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(body["errors"]).to include()
+        binding.pry
+      end
+    end
+  end
+
+end
