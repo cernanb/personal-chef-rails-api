0dd570e96039e4136b66e07ef8b3efdb3ecc8ff4
Cernan Bernardo <cernanb@gmail.com>
2017-08-12 21:04:31 -0700

    Add create action in Households Controller

--- app/controllers/api/v1/households_controller.rb
+++ app/controllers/api/v1/households_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::HouseholdsController < ApplicationController
-  before_action :authenticate_token!, only: [:index]
+  before_action :authenticate_token!, only: [:index, :create]
 
   def index
     @households = current_user.households
@@ -11,5 +11,22 @@ class Api::V1::HouseholdsController < ApplicationController
 
     render json: household
   end
+
+  def create
+    household = current_user.households.build(household_params)
+
+    if household.save
+      render json: household
+    else
+      render json: {
+        errors: household.errors.full_messages         
+      }, status: 500
+    end
+  end
+
+  private
+    def household_params
+      params.require(:household).permit(:name, :monthly_rate, :address)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :households, only: [:index, :show]
+      resources :households, only: [:index, :show, :create]
       resources :users, only: [:create]
       post '/auth', to: 'auth#login'
       post '/auth/refresh', to: 'auth#refresh'
