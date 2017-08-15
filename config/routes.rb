beadbec1e1b5390fed6701e442b893f0ccf5818f
Cernan Bernardo <cernanb@gmail.com>
2017-08-14 21:22:24 -0700

    Build routes for meals

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :households, only: [:index, :show, :create]
-      resources :users, only: [:create]
+      resources :households, only: [:index, :show, :create] do
+        resources :meals, only: [:index]
+      end
+      resources :users, only: [:create] 
       resources :meals, only: [:create, :index]
       post '/auth', to: 'auth#login'
       post '/auth/refresh', to: 'auth#refresh'
