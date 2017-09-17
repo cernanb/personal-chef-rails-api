a73d9ba75455eb2a622f7239706c5d7ea9693080
Cernan Bernardo <cernanb@gmail.com>
2017-09-16 19:32:12 -0700

    Add convert action to households controller

--- app/controllers/api/v1/households_controller.rb
+++ app/controllers/api/v1/households_controller.rb
@@ -8,23 +8,29 @@ class Api::V1::HouseholdsController < ApplicationController
 
   def show
     household = Household.find(params[:id])
-
+    
     render json: household
   end
-
+  
   def create
     household = current_user.households.build(household_params)
-
+    
     if household.save
       render json: household
     else
       render json: {
         errors: household.errors.full_messages         
-      }, status: 500
+        }, status: 500
+      end
     end
-  end
-
-  private
+    
+    def convert
+      household = Household.find(params[:household_id])
+      household.convert_to_client
+      render json: household
+    end
+    
+    private
     def household_params
       params.require(:household).permit(:name, :monthly_rate, :address)
     end
