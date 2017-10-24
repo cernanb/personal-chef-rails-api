a056ff3f3e64eb67ec32bebc844a678524b80b4e
Cernan Bernardo <cernanb@gmail.com>
2017-10-23 23:23:35 -0700

    Add association to household with notes

--- app/models/household.rb
+++ app/models/household.rb
@@ -6,6 +6,7 @@ class Household < ApplicationRecord
   has_many :members
   has_many :household_meals
   has_many :meals, through: :household_meals
+  has_many :notes
 
   def convert_to_client(rate)
     update(client?: true, monthly_rate: rate)
