c6c76853db47cbb2fdf8284493592ae155baa21a
Cernan Bernardo <cernanb@gmail.com>
2017-10-29 22:47:21 -0700

    Add create action for notes controller with route

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,3 +1,19 @@
 class Api::V1::NotesController < ApplicationController
+  def create
+    @household = Household.find(params[:household_id])
+    note = @household.notes.build(note_params)
+    
+    if note.save
+      render "households/show.json.jbuilder", household: @household
+    else
+      render json: {
+        errors: note.errors.full_messages
+        }, status: 500
+    end
+  end
 
+  private
+    def note_params
+      params.require(:note).permit(:content)
+    end
 end
