8f9aedbad7b07e9ec1e5e3487070a79943ac2de2
Cernan Bernardo <cernanb@gmail.com>
2017-11-17 20:58:09 -0800

    Build next_engagement method for household

--- app/models/household.rb
+++ app/models/household.rb
@@ -7,9 +7,14 @@ class Household < ApplicationRecord
   has_many :household_meals
   has_many :meals, through: :household_meals
   has_many :notes
+  has_many :engagements
 
   def convert_to_client(rate)
     update(client?: true, monthly_rate: rate)
+  end
+
+  def next_engagement
+    self.engagements.where('date > ?', Date.today).first
   end
 
 end
