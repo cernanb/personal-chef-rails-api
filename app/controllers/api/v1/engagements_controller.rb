5c2ca652d346bef4ce8199a7653b0af78af58c96
Cernan Bernardo <cernanb@gmail.com>
2017-11-17 20:57:32 -0800

    Build create action in engagements controller

---
+++ app/controllers/api/v1/engagements_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::EngagementsController < ApplicationController
+  def create
+    engagment = Engagement.new(engagement_params)
+
+    if engagement.save
+      render json: engagement
+    else
+      render json: {
+        errors: engagement.errors.full_messages
+        }, status: 500
+      end
+    end
+  end
+
+  def engagement_params
+    params.require(:engagement).permit(:date)
+  end
+end
