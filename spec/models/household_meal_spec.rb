350c3136c440570b100330f7dfd64c3231095d0c
Cernan Bernardo <cernanb@gmail.com>
2018-01-07 21:20:40 -0800

    Update validation tests for HouseholdMeal

--- spec/models/household_meal_spec.rb
+++ spec/models/household_meal_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe HouseholdMeal, type: :model do
   describe "validations" do
     it "requires a household" do
-      household_meal = build(:household_meal)
-      expect(household_meal).to be_valid
+      household_meal = build(:household_meal, household_id: nil)
+      expect(household_meal).to_not be_valid
+    end
+
+    it "requires a meal" do
+      household_meal = build(:household_meal, meal_id: nil)
+      expect(household_meal).to_not be_valid
     end
   end
 end
