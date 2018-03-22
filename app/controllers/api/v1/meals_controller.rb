fdaee4bb4bf44deb1960090992f5049cc0b31ee1
Cernan Bernardo <cernanb@gmail.com>
2018-03-21 23:25:25 -0700

    Build json for meals

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -15,7 +15,8 @@ class Api::V1::MealsController < ApplicationController
   def index
     @meals = Meal.all
 
-    render json: @meals
+    # render json: @meals
+    render "meals/index.json.jbuilder", meals: @meals
   end
 
   private
--- app/views/meals/index.json.jbuilder
+++ app/views/meals/index.json.jbuilder
@@ -1,3 +1,7 @@
 json.array! @meals do |meal|
-  
+  json.id meal.id
+  json.name meal.name
+  json.notes meal.meal_notes do |note|
+    note.content
+  end
 end
